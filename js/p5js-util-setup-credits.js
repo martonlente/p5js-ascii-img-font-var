94ef5a878c0773859a78ffe03c243341def69a93
Márton Lente <lentemarton@gmail.com>
2026-08-29 15:16:29 +0200

    Content update credits

--- js/p5js-util-setup-credits.js
+++ js/p5js-util-setup-credits.js
@@ -9,8 +9,8 @@ const p5jsSetupCredits = function() {
     Graphic design MA 1<br>
     Márton Lente, Győr 2026<br>
     <br>
-    p5js-sketch-utils v1.0.0-alpha<br>
-    <a href="https://github.com/martonlente/p5js-sketch-utils" target="_blank">Project on GitHub</a> – <a href="https://martonlente.com" target="_blank">martonlente.com</a>`;
+    p5js-ascii-img-font-var TODO<br>
+    <a href="https://github.com/martonlente/p5js-ascii-img-font-var" target="_blank">Project on GitHub</a> – <a href="https://martonlente.com" target="_blank">martonlente.com</a>`;
 
   helperP5jsUtils.appendChild(credits);
 }
